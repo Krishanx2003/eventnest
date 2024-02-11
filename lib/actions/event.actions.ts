42cb5f4986a0c51ec6718c122cf65f607d8b4480
Krishanx2003 <aiml21040@glbitm.ac.in>
2024-02-11 19:34:59 +0530

    testing commit

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -28,14 +28,12 @@ const populateEvent = (query: any) => {
 }
 
 // CREATE
-export async function createEvent({ userId, event, path }: CreateEventParams) {
+// CREATE
+export async function createEvent({ event, path }: CreateEventParams) {
   try {
     await connectToDatabase()
 
-    const organizer = await User.findById(userId)
-    if (!organizer) throw new Error('Organizer not found')
-
-    const newEvent = await Event.create({ ...event, category: event.categoryId, organizer: userId })
+    const newEvent = await Event.create({ ...event, category: event.categoryId })
     revalidatePath(path)
 
     return JSON.parse(JSON.stringify(newEvent))
